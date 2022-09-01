548a734f751dc0d1e03257c5c41a7b6b0bfe1f2f
Nikita Astaforkin <astaforkin90@gmail.com>
2022-09-01 12:14:42 +0300

    add folder tests, add calculator_test.py

---
+++ tests/calculator_test.py
@@ -0,0 +1,10 @@
+from threading import main_thread
+from tkinter.tix import MAIN
+from unicodedata import name
+from unittest import TestCase, main
+
+class CalculatorTest(TestCase):
+    pass
+
+if __name__ == '__main__':
+    main()   
