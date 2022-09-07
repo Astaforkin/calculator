631be1edc3e6b7e9c2a174c526c0b47f66e65046
Nikita Astaforkin <astaforkin90@gmail.com>
2022-09-07 11:45:35 +0300

    add test_no_sings() on pytest_calculator.py

--- test_calculator_pytest.py
+++ test_calculator_pytest.py
@@ -5,3 +5,12 @@ from calculator import calculator
 def test_fold():
     assert calculator('2 + 2') == 4
 
+def test_no_signs():
+        with pytest.raises(ValueError) as error:
+            calculator('abracadabra')
+        assert 'Выраженеие должно содержать хотя бы один знак (+-/*)' == error.value.args[0]
+
+
+
+if __name__ == '__main__':
+    pytest.main()
